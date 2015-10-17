0a8cfc6fbb5c6897ca68648a67fe06bdf658562f
Luke Heidecke <luke@solinea.com>
2015-10-16 20:20:34 -0700

    Unmatched quote

--- scripts/push-artifactory-develop.sh
+++ scripts/push-artifactory-develop.sh
@@ -3,7 +3,7 @@ set -ev
 
 registry="${DOCKER_REGISTRY_DEV}"
 image_base="${registry}/${NAME}"
-tag="${image_base}:${TRAVIS_BRANCH}-${COMMIT}
+tag="${image_base}:${TRAVIS_BRANCH}-${COMMIT}"
 
 exec docker tag "${NAME}:${COMMIT}" "${tag}"
 exec docker push "${tag}"
