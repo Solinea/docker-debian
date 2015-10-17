dd37f3bf7bdd4c53f70f49da8e740b8e4c5a326e
Luke Heidecke <luke@solinea.com>
2015-10-17 17:05:10 -0400

    Fixed tag/branch name for image push, cleaned ruby steps

--- scripts/push-artifactory.sh
+++ scripts/push-artifactory.sh
@@ -6,25 +6,25 @@ docker tag -f "${NAME}" "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_BRANCH}-${COMMI
 # Tag & Push 'branch-commit' tag to Development Registry
 docker push "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_BRANCH}-${COMMIT}"
 
-if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_TAG}" ]; then
+if [ "${TRAVIS_TAG}" ]; then
   # Tag & Push Version tag to Development Registry
-  docker tag -f "${NAME}" "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_TAG}"
-  docker push "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_TAG}"
+  docker tag -f "${NAME}" "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_BRANCH}"
+  docker push "${DOCKER_REGISTRY_DEV}/${NAME}:${TRAVIS_BRANCH}"
 
   # Tag & Push 'latest' tag to Development Registry
   docker tag -f "${NAME}" "${DOCKER_REGISTRY_DEV}/${NAME}:latest"
   docker push "${DOCKER_REGISTRY_DEV}/${NAME}:latest"
 
   # Tag & Push Version tag to Release Registry
-  docker tag -f "${NAME}" "${DOCKER_REGISTRY}/${NAME}:${TRAVIS_TAG}"
-  docker push "${DOCKER_REGISTRY}/${NAME}:${TRAVIS_TAG}"
+  docker tag -f "${NAME}" "${DOCKER_REGISTRY}/${NAME}:${TRAVIS_BRANCH}"
+  docker push "${DOCKER_REGISTRY}/${NAME}:${TRAVIS_BRANCH}"
 
   # Tag & Push 'latest' tag to Release Registry
   docker tag -f "${NAME}" "${DOCKER_REGISTRY}/${NAME}:latest"
   docker push "${DOCKER_REGISTRY}/${NAME}:latest"
 fi
 
-if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_TAG}" = "stable" ]; then
+if [ "${TRAVIS_BRANCH}" = "stable" ] && [ "${TRAVIS_TAG}" ]; then
   # Tag & Push 'stable' to Release Registry
   docker tag -f "${NAME}" "${DOCKER_REGISTRY}/${NAME}:stable"
   docker push "${DOCKER_REGISTRY}/${NAME}:stable"
